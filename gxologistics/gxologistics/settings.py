d70ed6fd89ac062c7f183215769bd434d5eb4af9
robkinson94 <129741120+robkinson94@users.noreply.github.com>
2024-12-06 13:14:23 +0000

    moved requirements.txt

--- gxologistics/gxologistics/settings.py
+++ gxologistics/gxologistics/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://gxologistics-metrics-tracker.onrender.com']
 CORS_ALLOWED_ORIGINS = ['http://localhost:5173']
 
 # Application definition
