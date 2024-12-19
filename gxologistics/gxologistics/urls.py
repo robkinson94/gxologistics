51c862587a1f3cdee9401462e93d60360fe59aff
robkinson94 <129741120+robkinson94@users.noreply.github.com>
2024-12-19 14:08:43 +0000

    csrf

--- gxologistics/gxologistics/urls.py
+++ gxologistics/gxologistics/urls.py
@@ -6,12 +6,12 @@ from core.views import ( CookieTokenObtainPairView, CookieTokenRefreshView, logo
 
 urlpatterns = [
     path("mis_admin_page/", admin.site.urls),
-    path("/", include("core.urls")),
+    path("api/", include("core.urls")),
     path('api/token/', CookieTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', CookieTokenRefreshView.as_view(), name='token_refresh'),
     path('api/logout/', logout_view, name='logout'),
     path('api/csrf/', get_csrf_token, name='get_csrf'),
-    path('api/me/', get_csrf_token, name='get_csrf'),
+    path('api/me/', get_csrf_token, name='me'),
 ]
 
 if not settings.DEBUG:
